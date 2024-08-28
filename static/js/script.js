1d3ac3eb2697627c7f3cbaa4c126acc9c35d8862
Eden <edench@post.bgu.ac.il>
2024-08-28 14:49:18 +0300

    Refactor form submission handling and result display logic
    - Refactored the form submission logic to handle server response more effectively.
    - Introduced `displayResult` and `displayError` helper functions to modularize result and error handling.
    - Implemented `setDownloadLink` function to manage the creation and assignment of the KML download link.
    - Improved readability and maintainability of the code by organizing logic into clear, reusable functions.

--- static/js/script.js
+++ static/js/script.js
@@ -1,56 +1,80 @@
-document.getElementById('path-form').addEventListener('submit',
-    async function (event) {
-        event.preventDefault();
+document.getElementById('path-form').addEventListener('submit', async function (event)
+{
+    // Preventing page reload
+    event.preventDefault();
 
-        // Get the values from the form fields
-        const startLat = document.getElementById('start-lat').value;
-        const startLon = document.getElementById('start-lon').value;
-        const endLat = document.getElementById('end-lat').value;
-        const endLon = document.getElementById('end-lon').value;
+    // Get the values from the form fields
+    const startLat = document.getElementById('start-lat').value;
+    const startLon = document.getElementById('start-lon').value;
+    const endLat = document.getElementById('end-lat').value;
+    const endLon = document.getElementById('end-lon').value;
 
-        try {
-            // Send the coordinates to the server in the expected format
-            const response = await fetch('http://127.0.0.1:5000/', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                    start: {lat: startLat, lon: startLon},
-                    end: {lat: endLat, lon: endLon}
-                })
-            });
+    // Prepare the request payload
+    const payload = {
+        start: {lat: startLat, lon: startLon},
+        end: {lat: endLat, lon: endLon}
+    };
 
-            // Get the JSON response
-            const result = await response.json();
+    try
+    {
+        // Send the coordinates to the server in the expected format
+        const response = await fetch('http://127.0.0.1:5000/', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(payload)
+        });
 
-            // Ensure the result container is displayed
-            document.getElementById('result-container').style.display = 'block';
+        // Get the JSON response
+        const result = await response.json();
 
-            if (!response.ok) {
-                document.getElementById('result').innerHTML = `
-                        <div class="result-block">
-<!--                            <h3>Result</h3>-->
-                            <p class="shortest-path text-danger">${result.error}</p>
-                        </div>
-                        `;
-            } else if (result.shortest_path) {
-                document.getElementById('result').innerHTML = `
-                <div class="result-block">
-                    <p class="shortest-path">Shortest Path: ${JSON.stringify(result.shortest_path)}</p>
-                    <a id="download-link" class="btn btn-success btn-download" download="shortest_path.kml">Download KML File</a>
-                </div>
-            `;
+        // Ensure the result container is displayed
+        const resultContainer = document.getElementById('result-container');
+        resultContainer.style.display = 'block';
 
-                // Create a Blob from the KML content and set the download link
-                const blob = new Blob([result.kml_content], {type: 'application/vnd.google-earth.kml'});
-                const url = URL.createObjectURL(blob);
-                document.getElementById('download-link').href = url;
-            } else {
-                document.getElementById('result').textContent = 'Error: ' + result.error;
-            }
-        } catch (error) {
-            console.error('Error:', error);
-            document.getElementById('result').textContent = 'An error occurred while finding the path.';
+        if (response.status !== 200)
+        {
+            // Display the error message
+            displayError(result.error);
+            return;
         }
-    });
+
+        // Display the result
+        displayResult(result);
+    }
+
+    catch (error)
+    {
+        console.error('Error:', error);
+        displayError('An error occurred while finding the path.');
+    }
+});
+
+function displayResult(result)
+{
+    const resultElement = document.getElementById('result');
+
+        resultElement.innerHTML = `
+            <div class="result-block">
+                <p class="shortest-path">Shortest Path: ${JSON.stringify(result.shortest_path)}</p>
+                <a id="download-link" class="btn btn-success btn-download" download="shortest_path.kml">Download KML File</a>
+            </div>
+        `;
+        setDownloadLink(result.kml_content);
+}
+
+function displayError(errorMessage)
+{
+    document.getElementById('result').innerHTML = `
+        <div class="result-block">
+            <p class="shortest-path text-danger">${errorMessage}</p>
+        </div>
+        `;
+}
+
+function setDownloadLink(kmlContent)
+{
+    const blob = new Blob([kmlContent], {type: 'application/vnd.google-earth.kml+xml'});
+    const url = URL.createObjectURL(blob);
+    const downloadLink = document.getElementById('download-link');
+    downloadLink.href = url;
+}
